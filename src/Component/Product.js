cca48769d3b6c18bdabfec7c1833cbc89b44a8b8
rohya-28 <rohitarsude28@gmail.com>
2022-12-02 13:17:09 -0400

    Routing

---
+++ src/Component/Product.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>Welcome To Product Page</div>
+  )
+}
+
+export default Product
---
+++ src/Component/Welcome.js
@@ -0,0 +1,6 @@
+const Welcome = () => {
+return (
+    <h1>Welcome To Page</h1>
+)
+}
+export default Welcome
