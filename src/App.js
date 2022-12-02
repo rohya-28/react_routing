e0aa65175a6b6f850b82eb80965e5baad05a70f2
rohya-28 <rohitarsude28@gmail.com>
2022-12-02 14:03:21 -0400

    Link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { Route, Routes } from 'react-router-dom' 
+import { Link, Route, Routes } from 'react-router-dom' 
 import Product from './Component/Product';
 import Welcome from './Component/Welcome';
 
 function App() {
   return (
-     <Routes> 
+    <>
+    <div>
+      <h1><Link to='/'>Home</Link></h1>
+      <h1><Link to='/Welcome'>Welcome</Link></h1>
+      <h1><Link to='/Product'>Product</Link></h1>
+    </div>
+      <Routes> 
       <Route path='Welcome' element={<Welcome />} />
       <Route path='Product' element={<Product />} />
       </Routes> 
+    </>
   );
 }
 
--- src/Component/Product.js
+++ src/Component/Product.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div>Welcome To Product Page</div>
+    <h1>This Product Page</h1>
   )
 }
 
