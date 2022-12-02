18e7bc1e3e40597597f036fe245518ae8c393a21
rohya-28 <rohitarsude28@gmail.com>
2022-12-02 15:30:35 -0400

    Extractiong Route Params

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductDetails = () => {
+  const params = useParams()
+
+  console.log(params.ProductId);
+
   return (
     <div>ProductDetails
-   
+       <p>{params.ProductId}</p>
     </div>
   )
 }
